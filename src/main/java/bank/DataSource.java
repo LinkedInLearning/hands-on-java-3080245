ecf19a8b9757840e41c395e6cc2160e43a0db5c0
Angie Jones <jones.angie@gmail.com>
2022-08-01 19:21:00 +0000

    construct Customer from data source

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,6 +2,8 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataSource {
@@ -12,15 +14,39 @@ public class DataSource {
 
     try{
       connection = DriverManager.getConnection(db_file);
-      System.out.println("we're connected");
     } catch(SQLException e){
       e.printStackTrace();
     }
     
     return connection;
   }
+
+  public static Customer getCustomer(String username) {
+    String sql = "select * from customers where username = ?";
+    Customer customer = null;
+
+    try(Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)){
+
+          statement.setString(1, username);
+          try(ResultSet resultSet = statement.executeQuery()){
+            customer = new Customer(
+              resultSet.getInt("id"),
+              resultSet.getString("name"),
+              resultSet.getString("username"),
+              resultSet.getString("password"),
+              resultSet.getInt("account_id"));
+          }
+
+    }catch(SQLException e){
+      e.printStackTrace();
+    }
+
+    return customer;
+  }
   
   public static void main(String[] args){
-    connect();
+    Customer customer = getCustomer("twest8o@friendfeed.com");
+    System.out.println(customer.getName());
   }
 }
