a3f324ae99bed212a0e7d1b5bdaf10b75a82ce5b
Angie Jones <jones.angie@gmail.com>
2022-08-01 18:33:12 +0000

    connect to data source

---
+++ src/main/java/bank/DataSource.java
@@ -0,0 +1,26 @@
+package bank;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataSource {
+  
+  public static Connection connect() {
+    String db_file = "jdbc:sqlite:resources/bank.db";
+    Connection connection = null;
+
+    try{
+      connection = DriverManager.getConnection(db_file);
+      System.out.println("we're connected");
+    } catch(SQLException e){
+      e.printStackTrace();
+    }
+    
+    return connection;
+  }
+  
+  public static void main(String[] args){
+    connect();
+  }
+}
