f929c7db1e5fef61d1cf1d7fa76469d566e4e1e8
Angie Jones <jones.angie@gmail.com>
2022-08-01 21:30:49 +0000

    construct Account from data source

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -46,8 +46,31 @@ public class DataSource {
     return customer;
   }
 
+  public static Account getAccount(int accountId) {
+    String sql = "select * from accounts where id = ?";
+    Account account = null;
+    try (
+      Connection connection = connect();
+      PreparedStatement statement = connection.prepareStatement(sql)) {
+        
+      statement.setInt(1, accountId);
+
+      try (ResultSet resultSet = statement.executeQuery()) {
+        account = new Account(
+            resultSet.getInt("id"),
+            resultSet.getString("type"),
+            resultSet.getDouble("balance"));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return account;
+  }
+
   public static void main(String[] args) {
     Customer customer = getCustomer("twest8o@friendfeed.com");
-    System.out.println(customer.getName());
+    Account account = getAccount(customer.getAccountId());
+    System.out.println(account.getBalance());
   }
 }
