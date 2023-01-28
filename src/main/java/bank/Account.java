4e38d7508a194941460914db8a5000a0764c6bf7
Lehlohonolo Letaoana <letaoana.lehlohonolo@outlook.com>
2023-01-28 21:18:00 +0000

    Added Account class

---
+++ src/main/java/bank/Account.java
@@ -0,0 +1,38 @@
+package bank;
+
+public class Account {
+  private int id;
+  private String type;
+  private double balance;
+
+  public Account(int id, String type, double balance){
+    setId(id);
+    setType(type);
+    setBalance(balance);
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getType() {
+    return this.type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public double getBalance() {
+    return this.balance;
+  }
+
+  public void setBalance(double balance) {
+    this.balance = balance;
+  }
+
+}
